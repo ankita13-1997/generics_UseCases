229b86c2587da560a60e7d092366dad9a2639cf6
ankita13-1997 <parhiankita@gmail.com>
2021-02-22 01:17:41 +0530

    the message of welcome is displayed

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+package generics;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("welcome to the showcase of generics method as compared with the normal way of solving the array problems");
+	}
+
+}
